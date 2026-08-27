ed678f7dbcce787fb7f166351a6a02b85cef85d6
matheusmatos199528-tech <matheus.matos199528@gmail.com>
2026-08-26 22:21:18 -0300

    a

--- pages/cadastro.php
+++ pages/cadastro.php
@@ -83,7 +83,7 @@
 
         
 
-        <button type="button" class="social-button google" aria-label="Entrar com Google" onclick="window.location.href='../api/oauth.php?provider=google'">
+        <button type="button" class="social-button google" aria-label="Entrar com Google" onclick="window.location.href='../oauth.php?provider=google'">
 
           <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="svg" aria-hidden="true">
             <path fill="#4285F4" d="M21.6 12.23c0-.71-.06-1.4-.18-2.07H12v3.92h5.38a4.6 4.6 0 0 1-2 3.02v2.54h3.24c1.9-1.75 2.98-4.33 2.98-7.41Z"/>
