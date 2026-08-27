468110399d04f9ffa0c3ba6cc74f13c036f4c241
matheusmatos199528-tech <matheus.matos199528@gmail.com>
2026-08-26 21:18:57 -0300

    ata

--- oauth.php
+++ oauth.php
@@ -1,16 +1,5 @@
 <?php
-<<<<<<< HEAD
- 
-declare(strict_types=1);
- 
-// Callback público mantido na raiz para corresponder à URL cadastrada no Google.
-require __DIR__ . '/api/oauth.php';
- 
- 
-=======
-
 declare(strict_types=1);
 
 // Callback público mantido na raiz para corresponder à URL cadastrada no Google.
 require __DIR__ . '/api/oauth.php';
->>>>>>> 361dc85f6f39aae9d13f402cb8e197d0fa65b43a
